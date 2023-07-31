123e9dd94ac94a56bd231c8eb4c6504f1b08f82e
Bogda <79489696+Bohdan1337max@users.noreply.github.com>
2023-07-31 14:19:38 +0200

    Develop

--- TicTacToe/GameState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TicTacToe;
-
-public class GameState
-{
-    public GameSigns[] GameField { get; set; } = null!;
-    public GameSigns CurrentSign { get; set; }
-    public int PointCoordinateX { get; set; }
-    public int PointCoordinateY { get; set; }
-}
